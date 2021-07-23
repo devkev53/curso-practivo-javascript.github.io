48168ded6d856c6645d5f29cad5f7f80619f33ee
Kevin Palma <kapalma05@gmil.com>
2021-07-22 23:22:06 -0600

    Se modifico el css y html de figuras geometricas, en donde se verifico que funcionara en dispositivos moviles y de escritorio.

--- js/figuras.js
+++ js/figuras.js
@@ -1,42 +1,139 @@
 // Capturamos los botones para llamar a la figura que desamos
-var btnCircle = document.querySelector('.circle');
-var btnSquare = document.querySelector('.square');
-var btnTriangle = document.querySelector('.triangle');
+var btnCircle = document.querySelector('.btnCirculo');
+var btnSquare = document.querySelector('.btnCuadrado');
+var btnTriangle = document.querySelector('.btnTriangulo');
 // Capturando todos los formularios de las figuras
 var shapes = document.querySelectorAll('.main_shapes_form');
-
-// Funcion que valida que se halla seleccionado el formualrio correcto
-function formShapeSelected(btn) {
-    let shape = btn.classList[0]
-    console.log(btn)
-    shapes.forEach(form => {
-        if (form.classList.contains(shape)) {
-            form.classList.remove('hiden');
-        } else {
-            form.classList.add('hiden');
-        }
-    });
-}
 
 // Haciendo click en circulo
 btnCircle.addEventListener('click', () => {
     btnCircle.classList.add('selected');
     btnSquare.classList.remove('selected');
     btnTriangle.classList.remove('selected');
-    formShapeSelected(btnCircle);
 })
 // Haciendo click en cuadrado
 btnSquare.addEventListener('click', () => {
     btnCircle.classList.remove('selected');
     btnSquare.classList.add('selected');
     btnTriangle.classList.remove('selected');
-    formShapeSelected(btnSquare);
 })
 // Haciendo click en triangulo
 btnTriangle.addEventListener('click', () => {
     btnCircle.classList.remove('selected');
     btnSquare.classList.remove('selected');
     btnTriangle.classList.add('selected');
-    formShapeSelected(btnTriangle);
-    //btnSquare.setAttribute('style', 'display:none');
 })
+// Programando la respuestas
+function sendPerimetro(p) {
+    $.alert({
+        title: 'Perimetro',
+        content: p,
+        type: 'blue',
+        theme: 'supervan',
+        typeAnimated: true,
+    });
+}
+function sendArea(a) {
+    $.alert({
+        title: 'Area',
+        content: a, 
+        type: 'blue',
+        theme: 'supervan',
+        typeAnimated: true,
+    });
+}
+function sendError() {
+    $.alert({
+        columnClass: 'small',
+        icon: 'fas fa-exclamation-square',
+        title: 'Error',
+        content: 'Debe de llenar los campos con datos validos..!', 
+        type: 'red',
+        theme: 'modern',
+        typeAnimated: true,
+    });
+}
+/* ----------------------------------------------------
+  +-+-+-+- HACIENDO LOS CALCULOS MATEMATICOS -+-+-+-+
+---------------------------------------------------- */
+
+/* ------------ Cuadrado ------------ */
+// Funcion que calcula el perimetro de un cuadrado
+function perimetroCuadrado(l) {
+    perimetro = l * 4;
+    return "El perimetro del cuadrado es: <b>" + perimetro + "cm</b>"; 
+}
+// Funcion que calcula el area de un cuadrado
+function areaCuadrado(l) {
+    area = l * l;
+    return "El area del cuadrado es: <b>" + area + "cm^2</b>"; 
+
+}
+// Interactuando con el HTML
+function calcularPerimetroCuadrado() {
+    let lado = document.getElementById('inputCuadrado');
+    if (parseFloat(lado.value)) {
+        console.log(typeof(parseFloat(lado.value)));
+        if (parseFloat(lado.value)>0) {
+            console.log('Numero');
+            let perimetro = perimetroCuadrado(parseFloat(lado.value));
+            sendPerimetro(perimetro);
+        }
+    }else {
+        console.log(typeof(parseFloat(lado.value)));
+        sendError()
+        lado.value = '';
+        return false;
+    }
+    lado.value = '';
+}
+function calcularAreaCuadrado() {
+    let lado = document.getElementById('inputCuadrado');
+    if (parseFloat(lado.value)) {
+        console.log(typeof(parseFloat(lado.value)));
+        if (parseFloat(lado.value)>0) {
+            console.log('Numero');
+            let perimetro = areaCuadrado(parseFloat(lado.value));
+        sendArea(perimetro);
+        }
+    }else {
+        console.log(typeof(parseFloat(lado.value)));
+        sendError()
+        lado.value = '';
+        return false;
+    }
+    lado.value = '';
+}
+/* ------------ Circulo ------------ */
+// Obteniendo PI
+const pi = Math.PI;
+// funcion que calcula el perimetro del circulo
+function diametroCirculo(r) {
+    return diametro = r * 2;
+}
+// funcion que calcula el perimetro del circulo
+function perimetroCirculo(r) {
+    var diametro = diametroCirculo(r);
+    var perimetro2 = pi * diametro;
+    perimetro2 = perimetro2.toFixed(2);
+    return "El perimetro del circulo es: <b>" + perimetro2 + "cm</b>"; 
+}
+
+// Interactuando con el HTML
+function calcularPerimetroCirculo() {
+    let radio = document.getElementById('inputRadio');
+    if (parseFloat(radio.value)) {
+        console.log(typeof(parseFloat(radio.value)));
+        if (parseFloat(radio.value)>0) {
+            console.log('Numero');
+            let perimetro = perimetroCirculo(parseFloat(radio.value));
+            sendPerimetro(perimetro);
+        }
+    }else {
+        console.log(typeof(parseFloat(radio.value)));
+        sendError()
+        radio.value = '';
+        return false;
+    }
+    radio.value = '';    
+}
