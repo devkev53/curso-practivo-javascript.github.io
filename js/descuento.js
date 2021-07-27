a6598e093846f24b547246ef330d183608a59681
Kevin Palma <kpalmar@miumg.edu.gt>
2021-07-27 12:05:32 -0600

    Se cumplio el reto de validar con cupones, por medio de una bonita animacion en un boton slider, que cambia el inpud de descuento a cupones, ademas se dio funcionamiento con js

--- js/descuento.js
+++ js/descuento.js
@@ -1,6 +1,52 @@
 const resultadoHtml = document.getElementById('resultPrecio');
 var porcent = document.getElementById('inputPorcent');
 const price = document.getElementById('inputPrice');
+var cupon = document.getElementById('inputCupon');
+var valueCupon = document.getElementById('inputCupon').value;
+var btnCupon = document.getElementById('cuponBtn');
+var form = document.querySelector('.formCalcular');
+var forCupon = false;
+const coupons = [
+    {
+        name:"JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name:"pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name:"es_un_secreto",
+        discount: 25,
+    }
+];
+
+const  isCouponValueValid = function(c) {
+    return c.name === cupon.value;
+}
+
+function userCoupon() {
+    return cuponValue = coupons.find(isCouponValueValid);
+}
+
+
+/*function cuponValid(ic) {
+    if (!coupons.includes(ic)) {
+        alert('El cupon ' + ic + ' no es valido')
+    } else if () {
+
+    } else if () {
+
+    } else if () {
+        
+    }
+}*/
+
+btnCupon.addEventListener('click', () => {
+    btnCupon.classList.toggle('active');
+    form.classList.toggle('cupon');
+    forCupon = true;
+});
 
 function valNum(e) {
     var dato = parseFloat(porcent.value);
@@ -12,11 +58,11 @@ function valNum(e) {
     }
 }
 
-function descuento() {
+function descuento(desc) {
     var price = parseFloat(document.getElementById('inputPrice').value);
-    var desc = parseFloat(document.getElementById('inputPorcent').value);
-    var porcentajeDescuento = 100 - desc;
+    var porcentajeDescuento = 100 - parseFloat(desc);
     var newPrice = (price * porcentajeDescuento) / 100;
+    console.log("Tipo: " + typeof(newPrice)+" Valor: "+ newPrice)
     if (isNaN(newPrice)) {
         alert('Verifica los datos ingresados..!')
         return false
@@ -27,9 +73,30 @@ function descuento() {
     return newPrice.toFixed(2);
 }
 
+function descuentoConCupon(cupon) {
+    if (isNaN(price.value)) {
+        alert('Llego Aqui.!')
+        alert('Verifica los datos ingresados..!');
+        return false;
+    } else {
+        return descuento(cupon.discount)   
+    } 
+}
 
 function calcularDescuento() {
-    var newPrice = descuento();
+    var newPrice;
+    var desc;
+    if (forCupon) {
+        if (!userCoupon()) {
+            alert("El cupon "+ valueCupon +" no es valido..!")
+        } else {
+            discount = userCoupon();
+            newPrice = descuentoConCupon(discount);
+        }
+    } else {
+        desc = parseFloat(document.getElementById('inputPorcent').value);
+        newPrice = descuento(desc);
+    }
     var response = '<h3>El nuevo precio es de: <b>' +
         newPrice +'</b></h3>'
     if (newPrice) {
